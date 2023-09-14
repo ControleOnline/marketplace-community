07647947bfd3515a747c6e23e24b308c01fa0800
Luiz Kim <luiz.kim@controleonline.com>
2023-09-14 16:12:57 -0300

    Update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,29 @@
-module.exports = {
-  presets: [
-    '@quasar/babel-preset-app'
-  ]
-}
+module.exports = (api) => {
+  api.cache(true);
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  const aliasPaths = {
+    // Alias de caminho padrão
+    "@controleonline": isProduction
+      ? "./node_modules/@controleonline"
+      : "./modules/controleonline",
+  };
+
+  return {
+    presets: [
+      "@quasar/babel-preset-app",
+      api.caller((caller) => caller && caller.target === "node")
+        ? { targets: { node: "current" } }
+        : {},
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: aliasPaths,
+        },
+      ],
+    ],
+  };
+};
